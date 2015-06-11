f1db74559016ab6ab65e68c7fce796cf59a5f1f8
Yuri Zats <yuri@zatsweb.com>
2015-06-11 00:09:53 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Learning [Django](https://github.com/django/django) with [Django tutorial](https://docs.djangoproject.com/en/1.8/intro/tutorial01/) to implement Web Polling app. 
+Learning [Django](https://github.com/django/django) with [Django tutorial](https://docs.djangoproject.com/en/1.8/intro/tutorial01/) to implement Web Polling app.
+
+To run the local server on port 8000:
+
+    python manage.py runserver 0.0.0.0:8000
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts   import render
+
+from django.shortcuts   import get_object_or_404, render
 from django.http        import HttpResponse
-from .models			import Question
+from .models            import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    response = "You are looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
-    
+
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s" % question_id)
-
